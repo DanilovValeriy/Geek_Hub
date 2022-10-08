38d64b71db15e3f57e478aa6ac66ed52d516ee42
DanilovValeriy <nashmultik@gmail.com>
2022-10-08 10:20:13 +0300

    HT2-3

---
+++ GeekHub_HT/HT_2/ht2_3.py
@@ -0,0 +1,7 @@
+'''Write a script which accepts a <number> from user 
+and print out a sum of the first <number> positive integers.'''
+
+my_number = int(input('Input number of numbers\n'))
+print(f'Sum of the first {my_number} positive integers = ', sum(range(1, my_number+1)))
+
+
