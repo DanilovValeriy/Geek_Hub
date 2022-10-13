4badac8888ee18cbba8e80b2008bfa2f14c1885e
DanilovValeriy <nashmultik@gmail.com>
2022-10-13 11:19:14 +0300

    HT3_3

---
+++ GeekHub_HT/HT_3/ht3_3.py
@@ -0,0 +1,15 @@
+'''Write a script to concatenate the following dictionaries to create a NEW one.
+    dict_1 = {'foo': 'bar', 'bar': 'buz'}
+    dict_2 = {'dou': 'jones', 'USD': 36}
+    dict_3 = {'AUD': 19.2, 'name': 'Tom'}'''
+
+
+dict_1 = {'foo': 'bar', 'bar': 'buz'}
+dict_2 = {'dou': 'jones', 'USD': 36}
+dict_3 = {'AUD': 19.2, 'name': 'Tom'}
+
+# Answer from atackowerflow
+d4 = dict(dict_1, **dict_2)
+d4.update(dict_3)
+
+print(d4)
