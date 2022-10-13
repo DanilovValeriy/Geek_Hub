4171311aa9333b4ee04f180c02346f48b4db1414
DanilovValeriy <nashmultik@gmail.com>
2022-10-13 12:25:45 +0300

    HT3_9 finish

--- GeekHub_HT/HT_2/ht2_2.py
+++ GeekHub_HT/HT_2/ht2_2.py
@@ -1,6 +1,7 @@
 '''Write a script which accepts two sequences of comma-separated colors from user. 
 Then print out a set containing all the colors from color_list_1 which are not present in color_list_2.'''
 
+
 first_seq = input('Input first sequences\n').split(',')
 second_seq = input('Input second sequences\n').split(',')
 
--- GeekHub_HT/HT_2/ht2_3.py
+++ GeekHub_HT/HT_2/ht2_3.py
@@ -3,5 +3,3 @@ and print out a sum of the first <number> positive integers.'''
 
 my_number = int(input('Input number of numbers\n'))
 print(f'Sum of the first {my_number} positive integers = ', sum(range(1, my_number+1)))
-
-
