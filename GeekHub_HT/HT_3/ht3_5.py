4b499c2d5bd5431e16f68cc025f4721bfc048e54
DanilovValeriy <nashmultik@gmail.com>
2022-10-13 11:30:26 +0300

    HT3_5

---
+++ GeekHub_HT/HT_3/ht3_5.py
@@ -0,0 +1,8 @@
+'''Write a script to remove values duplicates from dictionary. 
+Feel free to hardcode your dictionary.'''
+
+
+dict_1 = {'foo': 'bar', 'bar': 'buz', 'dou': 'jones', 'USD': 36, 'dou': 'jones', 'USD': 36}
+
+# truth be told, i don't understand how it work. I got the solution by accident
+print(dict(dict_1.items()))
