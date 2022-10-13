82143e48d05e7459b0b0d6db92d53e60920b7eed
DanilovValeriy <nashmultik@gmail.com>
2022-10-13 13:54:32 +0300

    HT3_6 update

--- GeekHub_HT/HT_3/ht3_6.py
+++ GeekHub_HT/HT_3/ht3_6.py
@@ -1,8 +1,14 @@
 '''Write a script to get the maximum and minimum VALUE in a dictionary.'''
 
+def python_rishaka(val):
+     if isinstance(val, (float, int)):
+          return val
+     else:
+          return len(val)
 
-dict_1 = {'foo': -2, 'bar': 23, 'dou': 333, 'USD': 36, 'dofu': 877, 'USD': 36, 'eee': 2}
+
+dict_1 = {'foo': 92, 'bar': 23, 'dou': 333, 'USD': 36, 'dofu': 877, 'USD': 36, 'eee': 'ggg', '11': {1}}
 
 
-print(f'Max element of dict = {max(dict_1.keys(), key=(lambda k: dict_1[k]))},\
-     min element of dict = {min(dict_1.keys(), key=(lambda k: dict_1[k]))}')
+print(f'Max element of dict = {max(dict_1.keys(), key=(lambda k: python_rishaka(dict_1[k])))},\
+     min element of dict = {min(dict_1.keys(), key=(lambda k: python_rishaka(dict_1[k])))}')
