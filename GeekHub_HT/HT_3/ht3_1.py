6b95c1eca7ab5a128c0c92b3975fd873a7580d9d
DanilovValeriy <nashmultik@gmail.com>
2022-10-13 13:38:30 +0300

    HT3_1 update

--- GeekHub_HT/HT_3/ht3_1.py
+++ GeekHub_HT/HT_3/ht3_1.py
@@ -3,13 +3,15 @@ The list of tuples can be hardcoded. The "replacement" value is entered by user.
 The number of elements in the tuples must be different.'''
 
 
-my_list = [(1,2), (1,2,3), (1,2,3,4), (1,2,3,4,5)]
+my_list = [(), (1,2), (1,2,3), (1,2,3,4), (1,2,3,4,5)]
 try:
     user_number = int(input('Input integer number\n'))
 except ValueError as err:
     print(f'Got error: {err}', 'Please input integer number', sep='\n')
 
 def change_value(my_arr, num):
+    if not my_arr:
+        return my_arr
     my_arr[-1] = num
     return my_arr
 # tried to use lambda function to change last element, but nothing worked
