d1f8e1020153f3b6e6744fd8a086fd334b3044ca
DanilovValeriy <nashmultik@gmail.com>
2022-10-17 21:52:50 +0300

    maybe repair ht3_5

--- GeekHub_HT/HT_3/ht3_5.py
+++ GeekHub_HT/HT_3/ht3_5.py
@@ -2,11 +2,11 @@
 Feel free to hardcode your dictionary.'''
 
 
-dict_1 = {'1': 1, '2': 2, 'f': 1, 'g': 2, 'r': 3}
+dict_1 = {'1': 1, '2': [2, 5], 'f': 1, 'g': 2, 'r': [2, 5], 77: {'e': 4}, 66: {'e': 4}}
 my_dict = {}
-my_set = set()
+my_arr = []
 for key, val in dict_1.items():
-    if val not in my_set:
-        my_set.add(val)
+    if val not in my_arr:
+        my_arr.append(val)
         my_dict.update({key: val})
 print(my_dict)
