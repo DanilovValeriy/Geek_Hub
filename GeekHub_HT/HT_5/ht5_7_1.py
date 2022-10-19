63850cde408b7e8bbf3dec18a91c4d60a155eb82
DanilovValeriy <nashmultik@gmail.com>
2022-10-19 20:07:28 +0300

    ht5_7 second version

--- GeekHub_HT/HT_5/ht5_7_1.py
+++ GeekHub_HT/HT_5/ht5_7_1.py
@@ -8,14 +8,19 @@
 my_list = [1, 1, 0, 0, 'foo',{3, 6, 9, 12}, [1, 2], True, False, 'foo', 1, [1, 2], [1, 2], [1, 2], {6: 6, 7: 7, 8: 8}, {3, 6, 9, 12}, {6: 6, 7: 7, 8: 8}]
 
 
+def counter(my_list, element):
+    if isinstance(element, (dict, list, set)):
+        return len(list(filter(lambda x: x == element, my_list)))
+    else:
+        return len(list(filter(lambda x: x is element, my_list)))
+
 
 def my_func(m_list):
-    m_list = list(map(str, m_list))
     list_of_values = []
     finish_list = []
     for el in m_list:
-        if el not in list_of_values:
-            finish_list.append(f'{el} --> {m_list.count(el)}')
+        if not counter(list_of_values, el):
+            finish_list.append(f'{el} --> {counter(m_list, el)}')
             list_of_values.append(el)
     return ', '.join(finish_list)
 
