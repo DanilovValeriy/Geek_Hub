3d84f7484e95b39539f63b3c448ef4dd07785ef1
DanilovValeriy <nashmultik@gmail.com>
2022-11-16 14:39:49 +0200

    HT11_3 classes ATM and Account

--- GeekHub_HT/HT_11/ht11_3.py
+++ GeekHub_HT/HT_11/ht11_3.py
@@ -1,7 +1,6 @@
 """Банкомат 4.0: переробіть программу з функціонального підходу програмування на використання класів.
 Додайте шанс 10% отримати бонус на баланс при створенні нового користувача.
 """
-
 import sqlite3
 
 db = sqlite3.connect('server.db')
@@ -43,23 +42,48 @@ denomination_of_banknotes = [1000, 500, 200, 100, 50, 20, 10]
 #
 # sql.executemany("INSERT INTO bankomat VALUES (?, ?)", BANKOMAT)
 # db.commit()
+class MyError(BaseException):
+    pass
 
 
 class Account:
     # Construct an Account object
-    def __init__(self, login, pin_code, cash_balance=0):
+
+    def working_with_atm(self):
+        if not login_in_account(self, self.login, self.pin_code):
+            raise MyError('Access denied. Wrong login or password')
+
+    def __init__(self, login, pin_code, user_balance=0):
         self.login = login
         self.pin_code = pin_code
-        self.cash_balance = cash_balance
+        self.user_balance = user_balance
 
     def login_in_account(self, login, pin_code):
         return self.login == login and self.pin_code == pin_code
 
-    def checking_cash_balance(self):
-        return self.cash_balance
+    def checking_balance_of_user(self):
+        return self.user_balance
 
-    def withdraw_checking_account(self, amount):
-        self.cash_balance -= amount
+    def withdraw_the_balance_of_user(self, amount):
+        if not isinstance(amount, (int, float)) and amount < 0:
+            return None
+        # elif
+        self.user_balance -= amount
 
-    def top_up_the_balance(self, amount):
-        self.cash_balance += amount
+    def top_up_the_balance_of_user(self, amount):
+        if not isinstance(amount, (int, float)) and amount < 0:
+            return None
+        self.user_balance += amount
+
+
+class ATM:
+    DENOMINATION = [1000, 500, 200, 100, 50, 20, 10]
+
+    def cash_in_atm(self):
+        pass
+
+    def top_up_the_balance_atm(self):
+        pass
+
+    def withdraw_the_balance_atm(self):
+        pass
