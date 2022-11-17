70e0e2406404ae0bc31122481e99d37300813345
DanilovValeriy <nashmultik@gmail.com>
2022-11-17 19:55:41 +0200

    HT11_3_2 complete redesign of the project

---
+++ GeekHub_HT/HT_11/11_3_2.py
@@ -0,0 +1,191 @@
+"""Банкомат 4.0: переробіть программу з функціонального підходу програмування на використання класів.
+Додайте шанс 10% отримати бонус на баланс при створенні нового користувача.
+"""
+
+import random
+import sqlite3
+
+db = sqlite3.connect('server.db')
+sql = db.cursor()
+
+denomination_of_banknotes = [1000, 500, 200, 100, 50, 20, 10]
+
+
+def logging(my_login, my_action):
+    sql.execute(f"INSERT INTO transactions (login, action) VALUES (?, ?)",
+                (my_login, my_action))
+    db.commit()
+
+
+class ATM:
+    def __init__(self, name, account_password, balance=0):
+        self.name = name
+        if ATM.check_password(account_password):
+            self.account_password = account_password
+
+        else:
+            raise Exception('Your password must contain more 5 characters and contain !@#$%^&')
+        self.balance = balance
+        sql.execute("INSERT INTO users VALUES (?, ?, ?, ?)", (self.name, self.account_password, balance, 0))
+        db.commit()
+
+    @staticmethod
+    def check_password(password):
+        if len(password) > 5 and set('!@#$%^&*').intersection(set(password)):
+            return True
+        else:
+            return False
+
+    @staticmethod
+    def check_number(amount):
+        try:
+            amount = int(amount)
+            if amount < 0:
+                print('The sum can not be less zero')
+                return False
+            else:
+                return int(amount)
+        except ValueError as err:
+            print(err)
+            return False
+
+    @staticmethod
+    def user_balance(login):
+        sql.execute(f"SELECT login, balance FROM users")
+        for el in sql.fetchall():
+            if el[0] == login:
+                return el[1]
+
+    @staticmethod
+    def check_user_in_system(my_login, my_password):
+        sql.execute("SELECT login, password FROM users")
+        if (my_login, my_password) in sql.fetchall():
+            print("Welcome to the system")
+            return True
+        else:
+            print("You don't have access to system")
+            print("Invalid login or password")
+            return False
+
+    def account_detail(self):
+        print("\n----------ACCOUNT DETAIL----------")
+        print(f"Account Holder: {self.name.upper()}")
+        print(f"Account Password: {self.account_password}")
+        print(f"Available balance: {self.balance}\n")
+
+    def deposit(self, amount):
+        if ATM.check_number(amount):
+            # lottery chance
+            lottery = random.randint(1, 1000)
+            if lottery % 10 == 0:
+                print('YOU WIN!!!\n CONGRATULATIONS!!!\n You got +10% to your amount\n')
+                self.balance = self.balance + amount * 1.1
+            else:
+                self.balance = self.balance + amount
+            sql.execute(f"UPDATE users set balance =? where login =?", (self.balance, self.name))
+            db.commit()
+            print(f'Your account has been topped up. The amount on the account is {self.balance}')
+            logging(self.name, f'Your account has been topped up. The amount on the account is {self.balance}')
+
+            print("Current account balance: ", self.balance)
+        else:
+            print(f'You can not put {amount}')
+
+    def withdraw(self, amount):
+        if not ATM.check_number(amount):
+            print(f'You can not withdraw {amount}')
+            return None
+        elif amount > self.balance:
+            print("Insufficient fund!")
+            print(f"Your balance is {self.balance} only.")
+            print("Try with lesser amount than balance.")
+            print()
+        else:
+            self.balance = self.balance - amount
+            sql.execute(f"UPDATE users set balance =? where login =?", (self.balance, self.name))
+            db.commit()
+            print(f'You have withdrawn {self.balance}. The amount in the account is {self.balance}')
+            logging(self.name, f'You have withdrawn {amount}. The amount in the account is {self.balance}')
+            print(f"{amount} withdrawal successful!")
+            print("Current account balance: ", self.balance)
+            print()
+
+    def check_balance(self):
+        sql.execute(f"SELECT login, balance FROM users")
+        for el in sql.fetchall():
+            if el[0] == self.name:
+                return el[1]
+
+    def transaction(self):
+        print("""
+            TRANSACTION 
+        *********************
+            Menu:
+            1. Account Detail
+            2. Check Balance
+            3. Deposit
+            4. Withdraw
+            5. Exit
+        *********************
+        """)
+        option = 0
+        while option != 6:
+            try:
+                option = int(input("Enter 1, 2, 3, 4, 5 or 6 for exit: "))
+            except:
+                print("Error: Enter 1, 2, 3, 4, 5 or 6 only!\n")
+                continue
+            else:
+                if option == 1:
+                    atm.account_detail()
+                elif option == 2:
+                    print(f'You got {atm.check_balance()}')
+                elif option == 3:
+                    amount = int(input("How much you want to deposit():"))
+                    atm.deposit(amount)
+                elif option == 4:
+                    amount = int(input("How much you want to withdraw():"))
+                    atm.withdraw(amount)
+                elif option == 5:
+                    print(f"""
+                printing receipt..............
+          ******************************************
+              Transaction is now complete.                         
+              Transaction number: {random.randint(10000, 1000000)} 
+              Account holder: {self.name.upper()}                  
+              Account password: {self.account_password}                
+              Available balance: {self.balance}                    
+
+              Thanks for choosing us as your bank                  
+          ******************************************
+          """)
+
+
+print("*******WELCOME TO BANK*******")
+
+choice = 0
+while choice not in {'x', 'X'}:
+    choice = input("Do you have an account? y/n. Press x or X to exit\n")
+    if choice == "y":
+        login = input('Input your login: ')
+        password = input('Input your password: ')
+        if ATM.check_user_in_system(login, password):
+            atm = ATM(login, password, ATM.user_balance(login))
+            atm.transaction()
+    elif choice == 'n':
+        choice = input("Do you want to create an account? y/n. Press x or X to exit\n")
+        if choice == 'y':
+            print("___________________________________________________________\n")
+            print("----------ACCOUNT CREATION----------")
+            login = input("Enter your name: ")
+            password = input("Enter your password MORE than 5 characters and contain !@#$%^: ")
+            atm = ATM(login, password)
+            atm.transaction()
+            print("Congratulations! Account created successfully......\n")
+
+print("""
+    -------------------------------------
+   | Thanks for choosing us as your bank |
+   | Visit us again!                     |
+    -------------------------------------
+        """)
--- GeekHub_HT/HT_11/ht11_3.py
+++ GeekHub_HT/HT_11/ht11_3.py
@@ -15,7 +15,6 @@ denomination_of_banknotes = [1000, 500, 200, 100, 50, 20, 10]
 # db.commit()
 
 
-#
 # sql.execute("""CREATE TABLE IF NOT EXISTS users (
 #             login TEXT,
 #             password TEXT,
@@ -56,7 +55,86 @@ class MyError(BaseException):
 
 
 class Account:
-    # Construct an Account object
+
+    @staticmethod
+    def start(login):
+        if login != 'admin':
+            print(f'Hi, {login}')
+            choice = 0
+            while choice != 6:
+                try:
+                    choice = int(input('Choice operation\n1. Look at the balance\n2. Top up the balance\n'
+                                       '3. Take the money\n4. View transaction history.\n5. Return to start menu\n6. '
+                                       'Exit\n'))
+                    match choice:
+                        case 1:
+                            print(look_balance(login))
+                            logging(login, 'Look at the balance')
+
+                        case 2:
+                            number = input('How much you want to top up the account?\n')
+                            if not check_number(number):
+                                continue
+                            else:
+                                change_balance(login, (int(number) // 10) * 10, '+')
+                                print(f'your chang = {int(number) % 10}')
+
+                        case 3:
+                            number = input('how much money you want to withdraw from the account?\n')
+                            if not check_number(number):
+                                continue
+                            elif cash_in_the_bankomat() > int(number):
+                                chang = int(number) % 10
+                                number = (int(number) // 10) * 10
+                                withdraw_from_atm(login, decomposition(number, [1000, 500, 200, 100, 50, 20, 10],
+                                                                       denomination_in_atm(False)))
+
+                                print(f'your chang = {chang}')
+                                change_balance(login, int(number), '-')
+
+                            else:
+                                print('There are not enough funds in the ATM')
+                                logging(login, 'There are not enough funds in the ATM')
+
+                        case 4:
+                            history_of_logging(login)
+                            logging(login, "View transaction history")
+
+                        case 6:
+                            print('Have a nice day. Good bye')
+                            logging(login, 'the end of working with an ATM')
+
+                        case 5:
+                            login_or_create()
+                            logging(login, 'Return to start menu')
+
+                except ValueError as err:
+                    print(err)
+                    return None
+        else:
+            print('Hello, Big Boss! Make your choice')
+            choice = 0
+            while choice != 5:
+                try:
+                    choice = int(input('Choice operation\n1. Denomination and balance ATM\n2. Change the balance ATM\n'
+                                       '3. History of using ATM\n4. Return to start menu.\n5. Exit\n'))
+
+                    match choice:
+                        case 1:
+                            print(f'Balance of the ATM {cash_in_the_bankomat()}')
+                            denomination_in_atm()
+                            logging('admin', 'Big boss looking the balance an ATM')
+                        case 2:
+                            change_bankomat_cash()
+
+                        case 3:
+                            history_of_logging('admin')
+
+                        case 4:
+                            login_or_create()
+
+                except ValueError as err:
+                    print(err)
 
     @staticmethod
     def check_password(password):
@@ -77,37 +155,21 @@ class Account:
         except TypeError('password must be string'):
             return f'User {login} with password {password} has not been created'
 
-    def working_with_atm(self):
-        if not Account.login_in_account(self, self.login, self.pin_code):
-            raise MyError('Access denied. Wrong login or password')
+    @staticmethod
+    def check_user_in_system(my_login, my_password):
+        sql.execute("SELECT login, password FROM users")
+        if (my_login, my_password) in sql.fetchall():
+            print("Welcome to the system")
+            return True
         else:
-            print('HALLO')
-
-    def __init__(self, login, pin_code, user_balance=0):
-        self.login = login
-        self.pin_code = pin_code
-        self.__user_balance = user_balance
-
-    # def user_connecting_to_db(self):
-    #     pass
-
-    def login_in_account(self, login, pin_code):
-        return self.login == login and self.pin_code == pin_code
-
-    @property
-    def user_balance(self):
-        return self.__user_balance
-
-    @user_balance.setter
-    def user_balance(self, amount):
-        if not Account.__check_number(amount):
-            raise MyError('Amount must be float or integer and more than 0.')
-        self.__user_balance += amount
+            print("You don't have access to system")
+            print("Invalid login or password")
+            return False
 
     def money_on_user_balance(self):
         sql.execute(f"SELECT login, balance FROM users")
         for el in sql.fetchall():
-            if el[0] == self.login:
+            if el[0] == self.name:
                 return el[1]
 
     @staticmethod
@@ -142,6 +204,7 @@ class ATM:
         pass
 
 # Account.create_account('Misha', 'gfg$$ghg', 1000)
+
 # print(ATM.cash_in_atm())
 # Account.create_account('Nikita', '12;:%№;3', 100)
 # Account.create_account('admin')
