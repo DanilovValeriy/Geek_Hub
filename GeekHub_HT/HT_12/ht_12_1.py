c5f8954434ff83d3f9045faeb6fac2fae4e34385
DanilovValeriy <nashmultik@gmail.com>
2022-11-19 17:15:01 +0200

    HT12_1 done

--- GeekHub_HT/HT_12/ht_12_1.py
+++ GeekHub_HT/HT_12/ht_12_1.py
@@ -9,3 +9,24 @@ class GeometricShapes:
     def change_color(self, new_color):
         self.color = new_color
 
+
+class Ellipse(GeometricShapes):
+
+    def __init__(self, semi_major_axis, semi_minor_axis, centre):
+        self.semi_major_axis = semi_major_axis
+        self.semi_minor_axis = semi_minor_axis
+        self.centre = centre
+
+
+class Square(GeometricShapes):
+
+    def __init__(self, side):
+        self.side = side
+
+
+my_ellips = Ellipse(10, 2, 0)
+my_square = Square(30)
+print(my_square.side)
+print(my_square.color)
+my_square.change_color('Black')
+print(my_square.color)
