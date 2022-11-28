512c07962b047c6888ce5b35f96835ed9bb3eb93
DanilovValeriy <nashmultik@gmail.com>
2022-11-28 19:46:35 +0200

    14_2 choose value

--- GeekHub_HT/HT_14/ht_14_2.py
+++ GeekHub_HT/HT_14/ht_14_2.py
@@ -12,6 +12,55 @@ class MyError(BaseException):
     pass
 
 
+def print_currency():
+    print('''Виберіть валюту, курс якої бажаєте подивитися\n
+            AUD	1	Австралійський долар
+            AZN	1	Азербайджанський манат
+            BYN	1	Білоруський рубль13
+            BGN	1	Болгарський лев
+            KRW	100	Вона
+            HKD	1	Гонконгівський долар
+            DKK	1	Данська крона
+            USD	1	Долар США
+            EUR	1	Євро
+            EGP	1	Єгипетський фунт
+            JPY	10	Єна
+            PLN	1	Злотий
+            INR	10	Індійська рупія
+            CAD	1	Канадський долар
+            HRK	1	Куна
+            MXN	1	Мексиканське песо
+            MDL	1	Молдовський лей
+            ILS	1	Новий ізраїльський шекель
+            NZD	1	Новозеландський долар
+            NOK	1	Норвезька крона
+            ZAR	1	Ренд
+            RUB	10	Російський рубль
+            RON	1	Румунський лей
+            IDR	1000	Рупія
+            SGD	1	Сінгапурський долар
+            XDR	1	СПЗ (спеціальні права запозичення)
+            KZT	100	Теньге
+            TRY	1	Турецька ліра
+            HUF	100	Форинт
+            GBP	1	Фунт стерлінгів
+            CZK	1	Чеська крона
+            SEK	1	Шведська крона
+            CHF	1	Швейцарський франк
+            CNY	1	Юань Женьміньбі
+            ''')
+
+
+def my_url_join(url):
+    URL = 'https://bank.gov.ua/ua/markets/exchangerates'
+    ADDITIONAL_URL = '-chart?cn%5B%5D='
+    return str(URL)[:-1] + ADDITIONAL_URL + url
+
+
+# print(requests.get(my_url_join('AZN')).text)
+print_currency()
+
+
 def check_data(my_data):
     try:
         result_dt = data.strptime(my_data, '%d.%m.%Y')
@@ -31,5 +80,4 @@ def check_data(my_data):
 
     return result_dt.strftime('%d.%m.%Y')
 
-
-print(check_data('81.10.1998'))
+# print(check_data('81.10.1998'))
